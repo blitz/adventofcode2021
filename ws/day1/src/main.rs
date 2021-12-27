d3c14edfdee5fc41b5cff48c8690b421465300e4
Julian Stecklina <js@alien8.de>
2021-12-27 09:32:35 +0100

    Solve day1

--- ws/day1/src/main.rs
+++ ws/day1/src/main.rs
@@ -1,27 +1,60 @@
 extern crate itertools;
 
-use std::io::{self, BufRead};
 use itertools::Itertools;
+use std::{
+    io::{self, BufRead},
+    ops::Add,
+};
 
-fn find_solution(values: &[i32], n: usize, expected_sum: i32) -> Option<i32>
-{
-    values.iter()
-	.map(|v| *v)
-	.combinations(n)
-	.filter(|v| v.iter().sum::<i32>() == expected_sum)
-	.next()
-	.map(|solution| solution.iter().fold(1, |acc, x| acc * x))
+fn to_three_value_window_sums<T: Add<Output = T> + Copy>(values: &[T]) -> Vec<T> {
+    values
+        .iter()
+        .copied()
+        .tuple_windows::<(T, T, T)>()
+        .map(|(a, b, c)| a + b + c)
+        .collect()
+}
+
+fn count_increases<T: Ord + Copy>(values: &[T]) -> usize {
+    values
+        .iter()
+        .copied()
+        .tuple_windows::<(T, T)>()
+        .filter(|(a, b)| a < b)
+        .count()
 }
 
 fn main() {
     let stdin = io::stdin();
 
-    let input_values: Vec<i32> = stdin.lock()
-	.lines()
-	.map(|l| l.expect("no I/O error").parse::<i32>().expect("integer input"))
-	.collect();
+    let input_values: Vec<i32> = stdin
+        .lock()
+        .lines()
+        .map(|l| {
+            l.expect("no I/O error")
+                .parse::<i32>()
+                .expect("integer input")
+        })
+        .collect();
 
-    for n in 2..4 {
-	println!("Solution for all {}-element choices : {}", n, find_solution(&input_values, n, 2020).expect("a solution"));
+    println!("Puzzle 1: {}", count_increases(&input_values));
+    println!(
+        "Puzzle 2: {}",
+        count_increases(&to_three_value_window_sums(&input_values))
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn three_value_window_sums_works() {
+        assert_eq!(to_three_value_window_sums(&[1, 2, 3, 4]), vec![6, 9]);
+    }
+
+    #[test]
+    fn count_increases_works() {
+        assert_eq!(count_increases(&[1, 1, 2, 2, 3]), 2);
     }
 }
